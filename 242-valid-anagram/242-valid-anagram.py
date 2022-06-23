2cecd2df9cef2a212daf66be32ceb9c380081ceb
help87668 <94478035+help87668@users.noreply.github.com>
2022-06-23 22:46:42 +0100

    Time: 74 ms (49.95%), Space: 15.1 MB (11.51%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,24 +1,14 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
         s = list(s)
         t = list(t)
-        
-        if len(s)!=len(t):
-            return False
-        else:
-            for i in s:
-                #print(i)
-                if i in t:
-                    t.remove(i)
-                    #print(t)
 
-        if t==[]:
-            return True
+        s.sort()
+        t.sort()
+
+        if (t==s):
+            return True 
         else:
             return False
         
