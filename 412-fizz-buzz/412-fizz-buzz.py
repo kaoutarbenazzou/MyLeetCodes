4290ef3a88216555a042908b71fe3f3bb4d3c7d7
kaoutarbenazzou <94478035+kaoutarbenazzou@users.noreply.github.com>
2022-06-28 13:35:29 +0100

    Time: 25 ms (95.11%), Space: 14.3 MB (36.57%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        answer=[]
+        for i in range(n):
+            i+=1
+            if i%3==0 and i%5==0:
+                answer.append('FizzBuzz')
+            elif i%3==0:
+                answer.append('Fizz')
+            elif i%5==0:
+                answer.append('Buzz')
+            else:
+                answer.append(str(i))
+            i-=1
+        return answer
+                
+                
+                
+        
