119f26c84f81c96ca056d7e0eedd785ef3a850fd
kaoutarbenazzou <94478035+kaoutarbenazzou@users.noreply.github.com>
2022-07-02 16:20:58 +0100

    Time: 62 ms (18.98%), Space: 13.9 MB (58.12%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,25 +1,7 @@
-class Solution(object):
-    def plusOne(self, digits):
-        """
-        :type digits: List[int]
-        :rtype: List[int]
-        """
-        # i=-1
-        # while i!=-len(digits):
-        #     k=i
-        def addi(digits, k=-1):
-            n=(digits[k]+1)%10
-            if (n!=0):
-                digits[k]+=1
-            else:
-                digits[k]=0
-                try:
-                    addi(digits, k-1)
-                except:
-                    digits.insert(0,0)
-                    addi(digits,k-1)
-        addi(digits)
-        return(digits)
-
-            
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        s = ''.join(str(e) for e in digits)
+        s=int(s)
+        s+=1
+        return list(str(s))
         
