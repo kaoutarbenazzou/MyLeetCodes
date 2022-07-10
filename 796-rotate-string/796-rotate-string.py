8c7482bc1a2b352d1287c763a61f773253845630
kaoutarbenazzou <94478035+kaoutarbenazzou@users.noreply.github.com>
2022-07-10 14:38:28 +0100

    Time: 38 ms (73.98%), Space: 13.8 MB (63.89%) - LeetHub

--- 796-rotate-string/796-rotate-string.py
+++ 796-rotate-string/796-rotate-string.py
@@ -1,20 +1,10 @@
-class Solution(object):
-    def rotateString(self, s, goal):
-        """
-        :type s: str
-        :type goal: str
-        :rtype: bool
-        """
-        s=list(s)
-        goal=list(goal)
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+
         for i in range(len(s)):
             if(s==goal):
                 return True
             else:
-                temp=s[0]
-                for j in range(len(s)-1):
-                    s[j]=s[j+1]
-                s[len(s)-1]=temp
-        return False
+                s=s[1:]+s[0]
                 
-        
+        return False
