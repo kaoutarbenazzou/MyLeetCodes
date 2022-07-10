3f5a6a37033bd00ff92db0189477ef8676d113dd
kaoutarbenazzou <94478035+kaoutarbenazzou@users.noreply.github.com>
2022-07-10 18:35:49 +0100

    Time: 27 ms (46.36%), Space: 13.9 MB (15.45%) - LeetHub

---
+++ 1592-rearrange-spaces-between-words/1592-rearrange-spaces-between-words.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def reorderSpaces(self, text):
+        """
+        :type text: str
+        :rtype: str
+        """
+        count=0
+        for i in text:
+            if i==' ':
+                count+=1
+        words=text.split()
+        l=len(words)
+        if(l==1):
+            return words[0]+(' '*count)
+        n=count//(l-1)
+        text=words[0]
+        del words[0]
+        for i in words:
+            text=text+(' '*n)+i
+        n=count-n*(l-1)
+        text=text+(' '*n)
+        return text
+            
+        
+        
+        
