44fe6944687a59847f065246785328eb35b21e02
kaoutarbenazzou <94478035+kaoutarbenazzou@users.noreply.github.com>
2022-07-11 13:21:00 +0100

    Time: 752 ms (5.05%), Space: 44.6 MB (8.26%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.py
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.py
@@ -5,14 +5,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        mx=-sys.maxint-1
-        mn=sys.maxint
+        # mx=-sys.maxint-1
+        # mn=sys.maxint
         div=1
-        for i in nums:
-            if i<mn:
-                mn=i
-            if i>mx:
-                mx=i
+        mx=max(nums)
+        mn=min(nums)
+        # for i in nums:
+        #     if i<mn:
+        #         mn=i
+        #     if i>mx:
+        #         mx=i
         for i in range(mn*mx,0,-1):
             if mx%i==0 and mn%i==0:
                 div=i
