3d70345f62d16e6ea0cbae9f47ea5f2c917879ed
kaoutarbenazzou <94478035+kaoutarbenazzou@users.noreply.github.com>
2022-07-11 13:10:25 +0100

    Time: 91 ms (5.48%), Space: 14 MB (35.89%) - LeetHub

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.py
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.py
@@ -1,10 +1,5 @@
-class Solution(object):
-    def numOfStrings(self, patterns, word):
-        """
-        :type patterns: List[str]
-        :type word: str
-        :rtype: int
-        """
+class Solution:
+    def numOfStrings(self, patterns: List[str], word: str) -> int:
         count=0
         for i in patterns:
             if i in word:
